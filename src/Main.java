6c0f1ba7cb052f66932081a85e5ce94a6c7b95dc
Sergey <springzepp@gmail.com>
2024-03-07 20:12:12 +0300

    Create program

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double sum = 1_000_000;
-        double interest = 9.99;
+        double interest = 0.0999;
         int month = 12;
         double monthlyPayment = service.calculate(sum, interest, month);
         System.out.println(monthlyPayment);
